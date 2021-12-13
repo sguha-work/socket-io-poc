b73de3a69733c0ac2657265e92ff2f6e191e2324
Sahasrangshu Guha <sahasrangshu.guha@ericsson.com>
2021-12-13 21:22:53 +0530

    changes

--- backend/routes/auction.js
+++ backend/routes/auction.js
@@ -1,12 +1,14 @@
+
 var express = require('express');
 var router = express.Router();
-
+var auctionService = require('./../services/auction_service');
 router.post('/set', function(req, res, next) {
   console.log(JSON.stringify(req));
   res.send('respond with a resource');
 });
 
 router.get('/get', function(req, res, next) {
+  console.log(auctionService.getLiveAuctionList());
   res.send('respond with a resource');
 });
 module.exports = router;
