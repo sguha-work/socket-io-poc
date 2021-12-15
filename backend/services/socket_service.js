cec4aedc3e5f4e53bf0e16d1be0af93eea3ebe91
Sahasrangshu Guha <sahasrangshu.guha@ericsson.com>
2021-12-15 16:43:23 +0530

    changes

--- backend/services/socket_service.js
+++ backend/services/socket_service.js
@@ -5,13 +5,7 @@ var carService = require("./car_service");
 var io, socketObj;
 var serverObj;
 var addListner = (socket) => {
-  // price update event listner
-  //socket.on(priceUpdateEventName, (data) => {
-  //console.log('price updated '+JSON.stringify(data));
-  //io.emit(priceUpdateEventName, data);
-  //});
   socket.on("bid enterred", (data) => {
-    console.log('got a bid'+JSON.stringify(data)); 
     let auction = new Auction({
       carId: data.carId,
       bidderId: data.bidderId,
@@ -25,8 +19,7 @@ var addListner = (socket) => {
           .updatePrice(data.carId, data.bidValue, data.bidderId)
           .then(() => {
             // emiting broadcast method by socket
-            console.log("price updated event triggered");
-            io.emit("price updated", data);
+            io.to("auction_room_" + data.carId).emit("price updated", data);
           })
           .catch((error) => {
             console.log('car price updation failed'+JSON.stringify(error));
@@ -49,8 +42,13 @@ module.exports = {
         },
       });
       io.on("connection", (socket) => {
-        socketObj = socket;
-        console.log("socket on ");
+        carService.get().then((cars)=>{
+          cars.forEach((car) => {
+            socket.join("auction_room_" + car['_id']);
+          });
+        }).catch(()=>{
+        });
+        socketObj = socket;        
         addListner(socket);
       });
     }
