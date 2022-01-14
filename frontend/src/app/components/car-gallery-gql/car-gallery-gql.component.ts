a9ddbe596191beeda5ef6ee5eb04ab7e2b8be508
Aritrik Das <aritrikdas@gmail.com>
2022-01-14 23:46:32 +0530

    frontend done

--- frontend/src/app/components/car-gallery-gql/car-gallery-gql.component.ts
+++ frontend/src/app/components/car-gallery-gql/car-gallery-gql.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {Apollo, gql} from 'apollo-angular';
-
+import { Apollo, gql, QueryRef } from 'apollo-angular';
+const sub = gql`subscription{
+  bidEntered {
+    carNumber,
+    currentBid
+  }
+}`;
+const dummy = gql`query{fakequery}`;
 @Component({
   selector: 'app-car-gallery-gql',
   templateUrl: './car-gallery-gql.component.html',
@@ -9,14 +15,24 @@ import {Apollo, gql} from 'apollo-angular';
 export class CarGalleryGqlComponent implements OnInit {
   public cars: any = [];
   public increseLimit = 10;
-  constructor(private apollo: Apollo) { }
+  private queryRef: QueryRef<any>;
+  constructor(private apollo: Apollo) {
+    this.queryRef = this.apollo.watchQuery({
+      query: dummy
+    });
+  }
 
   ngOnInit(): void {
+    this.getAllCars();
+    this.subscribeBidding();
+  }
+  private getAllCars() {
     this.apollo
       .watchQuery({
         query: gql`
         {
-          cars {
+          getCars {
+            id,
             carImage
             carName
             currentBid
@@ -27,20 +43,57 @@ export class CarGalleryGqlComponent implements OnInit {
         `,
       })
       .valueChanges.subscribe((result: any) => {
-        result.data.cars.forEach((car: any) => {
+        result.data.getCars.forEach((car: any) => {
           let carObj = {
             carImage: car.carImage,
             carName: car.carName,
             currentBid: car.currentBid,
             carNumber: car.carNumber,
-            basePrice: car.basePrice
+            basePrice: car.basePrice,
+            id: car.id
           }
           this.cars.push(carObj);
         });
-        console.log('this.cars',this.cars);
       });
   }
-  public setBidePrice() {
-
+  private subscribeBidding() {
+    this.queryRef.subscribeToMore({
+      document: sub,
+      updateQuery: (prev, { subscriptionData }) => {
+        let carData = subscriptionData.data.bidEntered;
+        this.updateCarInfo(carData);
+      }
+    })
   }
+  private updateCarInfo(carData: object) {
+    this.cars.forEach((car: any) => {
+      //@ts-ignore
+      if(car.carNumber == carData.carNumber) {
+        //@ts-ignore
+        car.currentBid = carData.currentBid;
+      }
+    });
+  }
+  public setBidPrice(id: String, currentBid: number) {
+    this.apollo.mutate({
+      mutation: gql`
+      mutation updateBidPrice($id: String!, $currentBid: Int!) {
+        updateBidPrice(id: $id, currentBid: $currentBid) {
+          carNumber
+          currentBid
+        }
+      }
+    `,
+      variables: {
+        id: id,
+        currentBid: currentBid + this.increseLimit
+      }
+    }).subscribe(({ data }) => {
+      console.log('got data', data);
+    }, (error) => {
+      console.log('there was an error sending the mutation', error);
+    });
+  }
+
+
 }
--- frontend/src/app/graphql.module.ts
+++ frontend/src/app/graphql.module.ts
@@ -1,12 +1,40 @@
 import {NgModule} from '@angular/core';
 import {APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
+import {ApolloClientOptions, InMemoryCache, split} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
+import {WebSocketLink} from '@apollo/client/link/ws';
+import {getMainDefinition} from '@apollo/client/utilities';
 
 const uri = 'https://localhost:3005/api'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  // Create an http link:
+  const http = httpLink.create({
+    uri: uri,
+  });
+
+  // Create a WebSocket link:
+  const ws = new WebSocketLink({
+    uri: `wss://localhost:3005/api`,
+    options: {
+      reconnect: true,
+    },
+  });
+
+  // using the ability to split links, you can send data to each link
+  // depending on what kind of operation is being sent
+  const link = split(
+    // split based on operation type
+    ({query}) => {
+      const data = getMainDefinition(query);
+      return (
+        data.kind === 'OperationDefinition' && data.operation === 'subscription'
+      );
+    },
+    ws,
+    http,
+  );
   return {
-    link: httpLink.create({uri}),
+    link: link,
     cache: new InMemoryCache(),
   };
 }
